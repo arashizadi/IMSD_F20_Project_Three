be46db2c8bf1eb03c4d883888c7d91b022b2ad50
Mykhal Parson <mykhal.reign@mail.citytech.cuny.edu>
2020-12-13 20:37:55 -0500

    Intro finished

--- IMSD_F20_Project_Three/Assets/Scripts/FadeOutText.cs
+++ IMSD_F20_Project_Three/Assets/Scripts/FadeOutText.cs
@@ -26,7 +26,8 @@ public class FadeOutText : MonoBehaviour
         }
         if (timer > 7)
         {
-            text.text = "Now I smoke a pack a day.";
+            text.text = "Now I smoke a pack a day, and it's been a hard habit to kick.";
+            text.fontSize = 28;
             
             if (text.alpha >= 1)
             {
@@ -37,13 +38,16 @@ public class FadeOutText : MonoBehaviour
             }
         }
 
-        if (timer > 11)
+        if (timer > 14)
         {
             text.alpha -= 0.05f;
         }
-        if (timer > 14)
+        if (timer > 15)
         {
-            text.text = "sentence 3.";
+            text.text = "I have an interview today with a large corporation. \n I'm stressed out.";
+
+            text.fontSize = 32;
+
             if (text.alpha >= 1)
             {
                 text.alpha = 1;
@@ -53,13 +57,13 @@ public class FadeOutText : MonoBehaviour
                 text.alpha += 0.05f;
             }
         }
-        if (timer > 18)
+        if (timer > 19)
         {
             text.alpha -= 0.05f;
         }
         if (timer > 21)
         {
-            text.text = "sentence 4.";
+            text.text = "I want to smoke before this interview.";
             if (text.alpha >= 1)
             {
                 text.alpha = 1;
@@ -69,13 +73,13 @@ public class FadeOutText : MonoBehaviour
                 text.alpha += 0.05f;
             }
         }
-        if (timer > 25)
+        if (timer > 26)
         {
             text.alpha -= 0.05f;
         }
-        if (timer > 28)
+        if (timer > 29)
         {
-            text.text = "sentence 5.";
+            text.text = "But, I NEED to get this job.";
             if (text.alpha >= 1)
             {
                 text.alpha = 1;
